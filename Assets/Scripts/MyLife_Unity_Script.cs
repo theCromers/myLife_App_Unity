5d17c17a04537e55ff78fa9208f609d13080644f
K. Cromer <70463509+theCromers@users.noreply.github.com>
2021-08-18 17:17:10 -0400

    assets commit

---
+++ Assets/Scripts/MyLife_Unity_Script.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyLife_Unity_Script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+       
+        print("My name is Kayla Cromer."); //code written 6.8.2021 @01:03 EDT
+        print(" ");
+        print("I was born in Atlanta, Georgia.");
+        print("");
+        print("My favorite video game right now is Warframe."); //highly recommend. I've sunk about 145 hours into it. 
+        print("");
+        print("I usually play games on a PC."); //converted from console. I haven't looked back.
+         
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
